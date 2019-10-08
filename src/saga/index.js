e96699bb487d4e2cfd3d092ade4e5280366eedee
bxy2000 <bxy_2000@163.com>
2019-10-08 15:02:05 +0800

    to do list

--- src/saga/index.js
+++ src/saga/index.js
@@ -2,7 +2,8 @@ import {call, put, takeEvery} from 'redux-saga/effects';
 import {LOAD_TODO_LIST, RENDER_TODO_LIST} from "../actions";
 
 export function* fetchToList() {
-    const endpoint = '';
+    const endpoint = 'https://gist.githubusercontent.com/brunokrebs/' +
+        'f1cacbacd53be83940e1e85860b6c65b/raw/to-do-items.json';
     const response = yield call(fetch, endpoint);
     const data = yield response.json();
     yield put({type: RENDER_TODO_LIST, toDoList: data});
